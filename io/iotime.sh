44c80a87cdba62d610e2e478e09d5223dc999c3f
Sagees <applebeef25@gmail.com>
2020-10-16 00:03:59 +0900

    modify timezone

--- io/iotime.sh
+++ io/iotime.sh
@@ -6,7 +6,7 @@ adb connect 192.168.0.14
 timestamp=$(date '+%Y/%m/%d/ %H:%M')
 
 while [ 1 ]; do
-	echo "$timestamp"
+	echo "$(date '+%Y/%m/%d/ %H:%M:%S')"
 	adb shell cat /proc/diskstats | grep "mmcblk0" | grep -v "mmcblk0[[:graph:]]" | awk '{print $3, $12, $14}'
-	sleep 1
+	sleep 2
 done
