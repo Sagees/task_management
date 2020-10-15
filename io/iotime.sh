6b7f19d774af8390a8c5f0696e5f0fbc442790d1
Sagees <applebeef25@gmail.com>
2020-10-15 23:51:42 +0900

    add io

---
+++ io/iotime.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+adb connect 192.168.0.14
+
+#timestamp=`date +%Y/%m/%d/ %H:%M`
+timestamp=$(date '+%Y/%m/%d/ %H:%M')
+
+while [ 1 ]; do
+	echo "$timestamp"
+	adb shell cat /proc/diskstats | grep "mmcblk0" | grep -v "mmcblk0[[:graph:]]" | awk '{print $3, $12, $14}'
+	sleep 1
+done
